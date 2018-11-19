ff5e57981b03198452137d800d4643faa8798a27
hafidzagung <hafidzagung46@gmail.com>
2018-11-19 17:39:18 +0700

    Test commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Rey
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pilih_menu;
+        System.out.println("================================ Menu ================================");
+        System.out.println("1.\tKurs Mata Uang Terkini");
+        System.out.println("2.\tKelola Kurs Mata Uang");
+        System.out.println("3.\tTransaksi");
+        System.out.println("4.\tKeluar");
+        System.out.println("======================================================================");
+        System.out.print("Pilih : ");
+        pilih_menu = sc.nextInt();
+        switch (pilih_menu) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
