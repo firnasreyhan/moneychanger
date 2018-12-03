5100c103a24c96a01fd9230955dcaa0e6a88e1c4
firnasreyhan <firnasreyhan@gmail.com>
2018-12-03 17:01:38 +0700

    UP

--- src/gui/config.java
+++ src/gui/config.java
@@ -13,19 +13,21 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
 public class config {
+
     private static Connection mysqlconfig;
-    public static Connection configDB()throws SQLException{
+
+    public static Connection configDB() throws SQLException {
         try {
-            String url="jdbc:mysql://localhost:3306/moneychanger"; //url database
-            String user="root"; //user database
-            String pass=""; //password database
+            String url = "jdbc:mysql://localhost:3306/moneychanger"; //url database
+            String user = "root"; //user database
+            String pass = ""; //password database
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
         } catch (Exception e) {
-            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+            System.err.println("koneksi gagal " + e.getMessage()); //perintah menampilkan error pada koneksi
         }
         return mysqlconfig;
-    }    
+    }
 }
